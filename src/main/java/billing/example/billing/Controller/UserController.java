5c6a1b7244feccf965649752fde02cde6c167014
Lavanya <lavny27@gmail.com>
2025-11-30 20:12:13 +0530

    Changed error

--- src/main/java/billing/example/billing/Controller/UserController.java
+++ src/main/java/billing/example/billing/Controller/UserController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
-public class Usercontrol
+public class UserController
 {
 
     @Autowired
--- src/main/java/billing/example/billing/config/CorsConfig.java
+++ src/main/java/billing/example/billing/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package billing.example.billing.config;
 
+import lombok.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,14 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    private String frontendUrl;
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://employeedeta.netlify.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
